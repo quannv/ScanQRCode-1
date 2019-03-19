901eec7570cdc8f80d2351b55f4c10bcca4a9269
TienDN <peago@PeagoMac.local>
2019-03-19 16:09:10 +0700

    tbusdriver

--- android/app/src/main/java/com/tbusdriver/MainApplication.java
+++ android/app/src/main/java/com/tbusdriver/MainApplication.java
@@ -3,12 +3,11 @@ package com.tbusdriver;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import org.reactnative.camera.RNCameraPackage;
-//import com.wix.RNCameraKit.RNCameraKitPackage;
+import com.wix.RNCameraKit.RNCameraKitPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import io.underscope.react.fbak.RNAccountKitPackage;
-//import org.reactnative.camera.RNCameraPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -29,12 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNCameraPackage(),
-            //new RNCameraKitPackage(),
+            new RNCameraKitPackage(),
             new VectorIconsPackage(),
             new RNI18nPackage(),
             new RNAccountKitPackage()
-            //new RNCameraPackage()
       );
     }
 
--- app/src/screen/scan-qrcode.screen.js
+++ app/src/screen/scan-qrcode.screen.js
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
 import { View, StyleSheet, Image, Text, TouchableOpacity } from "react-native";
-import QRCodeScanner from 'react-native-qrcode-scanner';
+//import QRCodeScanner from 'react-native-qrcode-scanner';
 import iconBack from "../../res/img/ic_left_arrow.png";
 import { sizeWidth } from "../helpers/size.helper";
+import { CameraKitCamera, CameraKitCameraScreen } from 'react-native-camera-kit'
 
 export default class ScanQRCodeScreen extends Component {
+
+  async  componentWillMount() {
+    const isCameraAuthorized = await CameraKitCamera.checkDeviceCameraAuthorizationStatus();
+    console.log(isCameraAuthorized);
+    if (isCameraAuthorized === -1) {
+      const isUserAuthorizedCamera = await CameraKitCamera.requestDeviceCameraAuthorization();
+    }
+  }
   onSuccess(e) {
     const onQRCode = this.props.navigation.getParam("onQRCode");
     if (onQRCode) {
-      onQRCode(e.data);
+      onQRCode(e.nativeEvent.codeStringValue);
       this.props.navigation.goBack();
     }
   }
 
   render() {
     return (
-      <QRCodeScanner
-        onRead={this.onSuccess.bind(this)}
+      <CameraKitCameraScreen
+        scanBarcode={true}
+        onReadCode={this.onSuccess.bind(this)}
+        colorForScannerFrame={'black'}
         topContent={
           <View style={styles.viewImage}>
             <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
